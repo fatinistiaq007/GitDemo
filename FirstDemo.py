5215546c622ada46f08cb5db47f0d533b2c201d5
fatinistiaq007 <fatin.istiaq@gmail.com>
2023-06-18 17:06:02 +0600

    commiit from asian architect guy

--- FirstDemo.py
+++ FirstDemo.py
@@ -9,3 +9,13 @@ print("value is", b+7)
 #test 45538565
 print("value is", a+1)
 print("value is", c+10)
+
+
+def getData(self):
+    print("I am now executing as method in class")
+def addition(self):
+    return self.first_num + self.second_num + self.num
+
+
+
+
--- demo.py
+++ demo.py
@@ -1,1 +1,6 @@
 print("Git learning purpose only")
+
+
+print("Dev branch...............")
+print(1+5)
+print("dfsdgjhdgfdhfdjhfgjhdf")
